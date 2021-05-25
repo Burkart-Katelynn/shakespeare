1ff29f1b71c4cfe08797a317b2e91cf661480895
Katelynn Burkart <katelynn.burkart@domo.com>
2021-05-24 20:54:23 -0600

    Dummy Review and ReviewCard components

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,34 +1,21 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getReviews } from "../../actions/reviews";
-import Review from "../../models/interfaces/review";
-import { selectReviews, selectReviewsLoading } from "../../reducers";
+import { selectReviewsLoading } from "../../reducers";
+import Reviews from "../reviews";
 import styles from "./index.cssmodule.scss";
 
 const App = () => {
   const dispatch = useDispatch();
-  const reviews = useSelector(selectReviews);
   const reviewsLoading = useSelector(selectReviewsLoading);
 
   useEffect(() => {
     dispatch(getReviews());
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const renderReviews = (reviews: Review[]) => {
-    if (reviews.length === 0) return <p>No reviews.</p>;
-
-    return (
-      <div>
-        {reviews.map((review) => (
-          <p key={review.id}>{review.author}</p>
-        ))}
-      </div>
-    );
-  };
-
   return (
     <div className={styles.app}>
-      {reviewsLoading ? <h1>Loading...</h1> : renderReviews(reviews)}
+      {reviewsLoading ? <h1>Loading...</h1> : <Reviews />}
     </div>
   );
 };
